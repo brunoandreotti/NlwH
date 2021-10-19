d1086d7cb5f3710a393d2e68e583238ec83b35ba
Bruno Andreotti <brunoandreotti@outlook.com>
2021-10-18 21:14:42 -0300

    Autenticação via github

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
+import "dotenv/config"
 import express from "express"
 
-const app = express()
+import { router } from './routes'
+
 const PORT = 4000
+const app = express()
+
+app.use(express.json())
+
+
+app.use(router)
+
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query
+
+  return res.json(code)
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
